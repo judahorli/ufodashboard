dc443dc0a51feafd80b9ddab2419b6008146487f
Judah Perillo <judahorli@gmail.com>
2022-05-20 20:49:32 -0400

    update to file url

--- ufosource/helpers.py
+++ ufosource/helpers.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 reports = pd.read_csv("https://media.githubusercontent.com/media/judahorli/big-ufo-data/main/nuforcData_2022.csv")
-us_county_city_mapdata = pd.read_csv("https://raw.githubusercontent.com/judahorli/big-ufo-data/main/us_cities_counties_mapdata.csv")
+us_county_city_mapdata = pd.read_csv("https://media.githubusercontent.com/media/judahorli/big-ufo-data/main/us_cities_counties_mapdata.csv")
